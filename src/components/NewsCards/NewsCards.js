7dd45d0f8724095f2c6a658dd86e342724954f79
Andrew Dillon <majik895@gmail.com>
2020-09-28 19:33:15 -0400

    final fixes

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -5,7 +5,7 @@ import NewsCard from './NewsCard/NewsCard';
 import useStyles from './styles.js';
 
 const infoCards = [
-  { color: '#cc5500', title: 'Latest News', text: 'Give me the latest news' },
+  { color: '#cc5500', title: 'Latest News', text: 'Give me the latest news  What does this app do?' },
   { color: '#3c1361', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news' },
   { color: '#b87333', title: 'News by Terms', info: 'George Clooney, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with Donald Trump' },
   { color: '#00308f', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
